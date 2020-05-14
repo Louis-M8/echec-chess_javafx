17a82643cad1ea10973bb92fb3b354f8db2d4615
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-05-14 07:45:17 -0400

    correction TP1

--- src/main/java/echec_chess_javafx/controleurs/ControleurAccueilFX.java
+++ src/main/java/echec_chess_javafx/controleurs/ControleurAccueilFX.java
@@ -8,10 +8,6 @@ import commun.debogage.J;
 
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f54eeef8a7ecfa7fb5c556d6e327c26c60fad913
 import commun_client.mvc.controleurs.FabriqueControleur;
 import echec_chess.modeles.difficultes.Parametres;
 import echec_chess_client.controleurs.ControleurAccueil;
